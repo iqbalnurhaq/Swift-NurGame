ebd0d551ff6733f25077ad3850c5ecaf7630b71b
Iqbal Nur Haq <iqbalnur833@gmail.com>
2022-11-06 07:08:01 +0700

    Initial Commit

---
+++ NurGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NurGame
+//
+//  Created by Iqbal Nur Haq on 06/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NurGame/NurGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurGameApp.swift
+//  NurGame
+//
+//  Created by Iqbal Nur Haq on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NurGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
